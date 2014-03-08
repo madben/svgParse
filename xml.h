02feb00f77a9e1e508278d7b7c50c61598735398
Benjamin Hansen <me@benjaminhansen.com>
2014-03-08 17:11:32 -0600

    Adding xml.h

---
+++ xml.h
@@ -0,0 +1,23 @@
+#indef _XML_H
+#define _XML_H
+
+struct xmlElememt {
+
+     char name[256];
+      
+
+};
+
+struct xmlAttribute {
+
+     char name[256];
+     char value[256];
+
+};
+
+
+
+
+
+
+#endif
